224b75d2a88094bb1e511007b6892e7362744fe3
Antoine Hanna <hannaantoinebusiness@gmail.com>
2026-06-03 12:03:56 +0200

    Automatic Single service creation
    We can only have single services for nom

--- Assets/Script/DepencyInjection/DependencyInjector.cs
+++ Assets/Script/DepencyInjection/DependencyInjector.cs
@@ -1,23 +1,41 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
-using NaughtyAttributes;
-using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DependencyInjector : MonoBehaviour
 {
-    private Dictionary<Type, MonoBehaviour> m_services = new Dictionary<Type, MonoBehaviour>();
+    private Dictionary<Type, Component> m_services = new Dictionary<Type, Component>();
     
     private void Awake()
     {
+        CreateSingleServices();
+        
         GameObject[] rootGameObjects = SceneManager.GetActiveScene().GetRootGameObjects();
         
-        GetServicesFromRoots(rootGameObjects);
+        //GetServicesFromRoots(rootGameObjects);
         
         InjectDependenciesFromRoots(rootGameObjects);
+    }
+
+    #region Services Search
+    private void CreateSingleServices()
+    {
+        foreach (TypeInfo typeInfo in GetType().Assembly.DefinedTypes)
+        {
+            if (typeInfo.HasAttribute<ServiceAttribute>())
+            {
+                Type serviceType = typeInfo.AsType();
+                GameObject go = new GameObject();
+                go.name = typeInfo.Name;
+                
+                Component createdService = go.AddComponent(serviceType);
+                
+                m_services.TryAdd(serviceType, createdService);
+            }
+        }
     }
 
     private void GetServicesFromRoots(GameObject[] rootGameObjects)
@@ -42,7 +60,9 @@ public class DependencyInjector : MonoBehaviour
 
         foreach (Transform childTransform in inGameObject.transform) CheckGameObjectForServices(childTransform.gameObject);
     }
+    #endregion
 
+    #region Injection
     private void InjectDependenciesFromRoots(GameObject[] rootGameObjects)
     {
         foreach (GameObject rootGameObject in rootGameObjects)
@@ -62,7 +82,7 @@ public class DependencyInjector : MonoBehaviour
             {
                 if (fieldInfo.HasAttribute<DependencyInjectionAttribute>())
                 {
-                    m_services.TryGetValue(fieldInfo.FieldType, out MonoBehaviour foundService);
+                    m_services.TryGetValue(fieldInfo.FieldType, out Component foundService);
 
                     if (foundService != null)
                     {
@@ -74,4 +94,5 @@ public class DependencyInjector : MonoBehaviour
         
         foreach (Transform childTransform in inGameObject.transform) InjectDependencies(childTransform.gameObject);
     }
+    #endregion
 }
