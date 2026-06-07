1fffa20fc3696e8f3e3d7951e299feee5654072d
Antoine Hanna <hannaantoinebusiness@gmail.com>
2026-06-07 10:00:56 +0200

    FIX field loop on monobehavior

--- Assets/Script/DepencyInjection/DependencyInjector.cs
+++ Assets/Script/DepencyInjection/DependencyInjector.cs
@@ -159,7 +159,7 @@ public class DependencyInjector : MonoBehaviour
                         }
                     }
                     
-                    if(foundNeededService) break;
+                    if(foundNeededService) continue;
                     
                     // II - Else start search in the hierarchy
                     // Start search in parents for injector component with children scope
@@ -192,7 +192,7 @@ public class DependencyInjector : MonoBehaviour
                         else if (parentDistance <= childDistance) fieldInfo.SetValue(component, serviceFoundInParent);
                         else fieldInfo.SetValue(component, serviceFoundInChildren);
                         
-                        break;
+                        continue;
                     }
                     
                     // III - Else search in from other roots for injector component with scene scope
@@ -206,7 +206,7 @@ public class DependencyInjector : MonoBehaviour
                     if (foundNeededService)
                     {
                         fieldInfo.SetValue(component, serviceFoundInScene);
-                        break;
+                        continue;
                     }
                     // IV - At this point we didn't find any, we just continue
                 }
